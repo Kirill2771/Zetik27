837acef0be8c7f386112e0191b13f13017cd3287
zetik27 <sedovkirill541@gmail.com>
2024-10-10 15:14:32 +0000

    11

---
+++ 8.py
@@ -0,0 +1,7 @@
+age = input("Введите ваш возраст: ")
+gander = input("Введите пол пользователя: ")
+name = input("Введите свое имя: ")
+city = input("Введите город ваш: ")
+numberclass = input("Введите ваш класс обучения: ")
+a=[age,gander, name, city,numberclass]
+print(a)
